2d6911dfc20ffe4fc22d66ec8b5fb95459a93cef
VV4st3d <guvxzz@gmail.com>
2026-02-20 06:16:19 +0300

    feat(chat): added preview for attached image

--- app/components/Chat/ChatFooter.vue
+++ app/components/Chat/ChatFooter.vue
@@ -6,6 +6,7 @@ const messageText = defineModel<string>();
 const FILE_INPUT = 'file-input';
 
 const fileInput = useTemplateRef<HTMLInputElement>(FILE_INPUT);
+const imageUrl = ref<string | null>(null);
 
 const attachedFile = ref<File | null>(null);
 
@@ -20,8 +21,20 @@ const handleOpenFileUploader = () => {
 
 const handleSetFile = (e: Event) => {
   const input = e.target as HTMLInputElement;
-  if (input.files && input.files[0]) {
-    attachedFile.value = input.files[0];
+  const file = input.files?.[0];
+  if (file) {
+    attachedFile.value = file;
+    if (attachedFile.value.type.startsWith('image')) {
+      imageUrl.value = URL.createObjectURL(attachedFile.value);
+    }
+  }
+};
+
+const clearFile = () => {
+  attachedFile.value = null;
+  if (imageUrl.value) {
+    URL.revokeObjectURL(imageUrl.value);
+    imageUrl.value = null;
   }
 };
 
@@ -30,43 +43,62 @@ const sendMessage = () => {
     return emit('send-message');
   }
   emit('upload-file', attachedFile.value);
+  clearFile();
 };
 </script>
 
 <template>
   <footer
-    class="chat-input-container flex items-center gap-2 p-3 bg-[var(--bg-secondary)] border-t border-[var(--border)]"
+    class="chat-input-container flex-col border-[var(--border)] bg-[var(--bg-secondary)]"
   >
-    <button
-      class="p-2 rounded-full hover:bg-[var(--bg-tertiary)] transition"
-      @click="handleOpenFileUploader"
+    <div
+      v-if="attachedFile"
+      class="relative inline-block mb-3 bg-[var(--bg-tertiary)] rounded-lg h-20 w-20"
     >
-      <Icon name="paper-clip" size="24" />
-    </button>
+      <img
+        v-if="imageUrl"
+        :src="imageUrl"
+        class="h-full w-full object-cover rounded-md border border-[var(--border)]"
+      >
+      <div v-else class="flex items-center gap-2 text-sm">
+        <Icon name="document" size="20" />
+        <span class="truncate max-w-[150px]">{{ attachedFile.name }}</span>
+      </div>
 
-    <input
-      :ref="FILE_INPUT"
-      type="file"
-      accept="image/*,video/*,.pdf"
-      class="hidden"
-      @change="handleSetFile"
-    >
+      <button
+        class="absolute -top-1 -right-1 transition deleteFile"
+        @click="clearFile"
+      >
+        <Icon name="x-mark" size="16" />
+      </button>
+    </div>
+    <div class="flex items-center gap-4">
+      <button @click="handleOpenFileUploader">
+        <Icon name="paper-clip" size="24" />
+      </button>
+      <input
+        :ref="FILE_INPUT"
+        type="file"
+        accept="image/*,video/*,.pdf"
+        class="hidden"
+        @change="handleSetFile"
+      >
 
-    <textarea
-      v-model="messageText"
-      placeholder="Напишите сообщение..."
-      rows="1"
-      class="flex-1 resize-none bg-[var(--bg-tertiary)] border border-[var(--border)] rounded-[var(--radius)] px-4 py-3 focus:outline-none focus:border-[var(--accent)] focus:ring-2 focus:ring-[var(--accent)]/30 transition"
-      @input="emit('start-typing')"
-      @keydown.enter.exact.prevent="sendMessage"
-    />
+      <textarea
+        v-model="messageText"
+        placeholder="Напишите сообщение..."
+        rows="1"
+        class="flex-1 resize-none bg-[var(--bg-tertiary)] border border-[var(--border)] rounded-[var(--radius)] px-4 py-3 focus:outline-none focus:border-[var(--accent)] focus:ring-2 focus:ring-[var(--accent)]/30 transition"
+        @input="emit('start-typing')"
+        @keydown.enter.exact.prevent="sendMessage"
+      />
 
-    <button
-      :disabled="!messageText?.trim()"
-      class="p-2 rounded-full disabled:opacity-50 disabled:cursor-not-allowed hover:bg-[var(--accent)]/10 transition"
-      @click="sendMessage"
-    >
-      <Icon name="paper-airplane" size="24" />
-    </button>
+      <button
+        :disabled="!messageText?.trim() && !attachedFile"
+        @click="sendMessage"
+      >
+        <Icon name="paper-airplane" size="24" />
+      </button>
+    </div>
   </footer>
 </template>
